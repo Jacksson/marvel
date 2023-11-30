c66ed098cf6d6cf5ac618b81fdf934b2692a937e
Jackson Enrique Mosquera Rivas <jackson.mosquera@radartechnologies.net>
2023-11-30 09:11:46 -0500

    Backend -> Habilito modelo de seguridad Basic, en otros escenarios usaria JWT u OAUTH.

---
+++ microservices/characters/src/main/java/co/jackson/marvel/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package co.jackson.marvel.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .securityMatcher("/api/**")
+            .authorizeHttpRequests((authz) -> authz
+                //.requestMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    // @formatter:off
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    // @formatter:on
+
+}
